42470e57e9374aeb7c5abe59c27508ba30dfca39
MohamedKhaled201841 <86727231+MohamedKhaled201841@users.noreply.github.com>
2021-09-12 21:26:45 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a demo projec[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cp a fictional IT institude to choose its courses better to coup with new technologies which keep evolving quickly',
+    author='Mohamed Khaled (ITI Alex)',
+    license='',
+)
